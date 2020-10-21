42e834a73c007c5f1b26387a252261a178885602
Nikita Gaidakov <ngaidakov@podfather.com>
2020-10-21 22:33:11 +0100

    Fix drawn rect length, and a big tidyup

--- src/Main.elm
+++ src/Main.elm
@@ -20,7 +20,7 @@ import Rectangle3d
 import SketchPlane3d exposing (SketchPlane3d)
 import Svg.Styled
 import Svg.Styled.Attributes as SvgAttr
-import Vector2d
+import Vector2d exposing (Vector2d)
 import Vector3d
 
 
@@ -37,17 +37,31 @@ main =
         }
 
 
+type alias Plane =
+    SketchPlane3d Length.Meters World { defines : TopLeftCoordinates }
+
+
+type alias PlanePoint =
+    Point2d Length.Meters TopLeftCoordinates
+
+
+type alias Rect =
+    Rectangle2d Length.Meters TopLeftCoordinates
+
+
 type alias Model =
     { rects : List (Rectangle2d Length.Meters TopLeftCoordinates)
-    , plane : SketchPlane3d Length.Meters World { defines : TopLeftCoordinates }
-    , viewPlane : SketchPlane3d Length.Meters World { defines : TopLeftCoordinates }
-    , newRect :
-        Maybe
-            { originPoint : Point2d Length.Meters TopLeftCoordinates
-            , initialX : Float
-            , length : Length.Length
-            }
+    , sourcePlane : Plane
+    , viewPlane : Plane
+    , drawnRect :
+        Maybe DrawnRect
     , lockY : Bool
+    }
+
+
+type alias DrawnRect =
+    { originPoint : PlanePoint
+    , rect : Rect
     }
 
 
@@ -68,36 +82,16 @@ init () =
         , ( 18, 16, 3 )
         , ( 18, 2, 2 )
         ]
-            |> List.map (\( x, y, length ) -> toRectangle (Point2d.centimeters x y) (Length.centimeters length))
-    , plane = SketchPlane3d.xy
+            |> List.map (\( x, y, length ) -> rectFrom (Point2d.centimeters x y) (Point2d.centimeters (x + length) 0))
+    , sourcePlane = SketchPlane3d.xy
     , viewPlane =
         SketchPlane3d.xy
             |> SketchPlane3d.offsetBy (Length.meters -3)
-            |> SketchPlane3d.rotateAround tiltAxis (Angle.degrees 15)
-    , newRect = Nothing
-    , lockY = False
+            |> SketchPlane3d.rotateAround tiltAxis (Angle.degrees 20)
+    , drawnRect = Nothing
+    , lockY = True
     }
         |> withNoCmd
-
-
-toRectangle : Point2d Length.Meters TopLeftCoordinates -> Length.Length -> Rectangle2d Length.Meters TopLeftCoordinates
-toRectangle leftMidPoint length =
-    let
-        yTranslation =
-            Vector2d.centimeters 0 0.75
-
-        xTranslation =
-            Vector2d.xy length <| Length.meters 0
-
-        topLeft =
-            leftMidPoint
-                |> Point2d.translateBy (Vector2d.reverse yTranslation)
-
-        bottomRight =
-            leftMidPoint
-                |> Point2d.translateBy (yTranslation |> Vector2d.plus xTranslation)
-    in
-    Rectangle2d.from topLeft bottomRight
 
 
 subscriptions : Model -> Sub Msg
@@ -117,28 +111,30 @@ update msg model =
 
             MouseDown x y ->
                 { model
-                    | newRect =
-                        plotPointAt x y model.plane model.viewPlane
+                    | drawnRect =
+                        ( x, y )
+                            |> plottedOn model.sourcePlane model.viewPlane
                             |> Maybe.map
                                 (\point ->
                                     { originPoint = point
-                                    , initialX = x
-                                    , length = Length.centimeters 0.1
+                                    , rect = rectFrom point point
                                     }
                                 )
                 }
 
             MouseMove x y ->
                 { model
-                    | newRect =
-                        model.newRect
-                            |> Maybe.map (\rect -> { rect | length = Length.cssPixels <| x - rect.initialX })
+                    | drawnRect =
+                        Maybe.map2
+                            (\rect endPoint -> { rect | rect = rectFrom rect.originPoint endPoint })
+                            model.drawnRect
+                            (( x, y ) |> plottedOn model.sourcePlane model.viewPlane)
                 }
 
             MouseUp x y ->
                 { model
-                    | newRect = Nothing
-                    , rects = includeNewRect model.newRect model.rects
+                    | drawnRect = Nothing
+                    , rects = includeDrawnRect model.drawnRect model.rects
                 }
 
             Wheel deltaX deltaY ->
@@ -150,8 +146,8 @@ update msg model =
                         Angle.degrees <| deltaY * wheelCoefficient
                 in
                 { model
-                    | plane =
-                        model.plane
+                    | sourcePlane =
+                        model.sourcePlane
                             |> SketchPlane3d.rotateAround turnAxis offsetAngleX
                     , viewPlane =
                         model.viewPlane
@@ -164,8 +160,12 @@ update msg model =
                 }
 
 
+
+-- VIEW
+
+
 view : Model -> List (Html Msg)
-view ({ plane, viewPlane, rects, newRect } as model) =
+view ({ sourcePlane, viewPlane, rects, drawnRect } as model) =
     List.map Styled.toUnstyled <|
         [ Styled.button
             [ StyledEvents.onClick ToggleLockY
@@ -182,8 +182,8 @@ view ({ plane, viewPlane, rects, newRect } as model) =
                     "Lock Y"
             ]
         , Styled.br [] []
-        , includeNewRect newRect rects
-            |> viewRects plane viewPlane
+        , includeDrawnRect drawnRect rects
+            |> viewRects sourcePlane viewPlane
             |> Svg.Styled.svg
                 [ SvgAttr.width "1200px"
                 , SvgAttr.height "1200px"
@@ -195,6 +195,7 @@ view ({ plane, viewPlane, rects, newRect } as model) =
         ]
 
 
+viewRects : Plane -> Plane -> List Rect -> List (Svg.Styled.Svg msg)
 viewRects plane viewPlane =
     List.map
         (Rectangle3d.on plane
@@ -204,7 +205,7 @@ viewRects plane viewPlane =
         )
 
 
-viewPolygon : List (Point2d Length.Meters TopLeftCoordinates) -> Svg.Styled.Svg msg
+viewPolygon : List PlanePoint -> Svg.Styled.Svg msg
 viewPolygon points =
     Svg.Styled.polygon
         [ SvgAttr.points <| geometryToSvgPoints points
@@ -213,7 +214,7 @@ viewPolygon points =
         []
 
 
-geometryToSvgPoints : List (Point2d Length.Meters TopLeftCoordinates) -> String
+geometryToSvgPoints : List PlanePoint -> String
 geometryToSvgPoints =
     List.map
         (Point2d.toRecord Length.inCssPixels
@@ -222,40 +223,55 @@ geometryToSvgPoints =
         >> String.join " "
 
 
-plotPointAt :
-    Float
-    -> Float
-    -> SketchPlane3d Length.Meters coordinates { defines : local }
-    -> SketchPlane3d Length.Meters coordinates { defines : world }
-    -> Maybe (Point2d Length.Meters local)
-plotPointAt x y plane viewPlane =
+
+-- GEOMETRY
+
+
+rectFrom : PlanePoint -> PlanePoint -> Rect
+rectFrom originPoint endPoint =
+    let
+        halfHeight =
+            Vector2d.centimeters 0 0.7
+
+        length =
+            Vector2d.from originPoint endPoint
+                |> Vector2d.xComponent
+                |> flip Vector2d.xy (Length.meters 0)
+
+        topLeft =
+            originPoint
+                |> Point2d.translateBy (Vector2d.reverse halfHeight)
+
+        bottomRight =
+            originPoint
+                |> Point2d.translateBy (halfHeight |> Vector2d.plus length)
+    in
+    Rectangle2d.from topLeft bottomRight
+
+
+plottedOn : Plane -> Plane -> ( Float, Float ) -> Maybe PlanePoint
+plottedOn sourcePlane viewPlane ( x, y ) =
     let
         translationVector =
             Point2d.pixels x y
-                |> Point2d.at_
-                    (Pixels.pixels 96
-                        |> Quantity.per (Length.inches 1)
-                    )
+                |> Point2d.at_ pixelDensity
                 |> Point3d.on viewPlane
                 |> Vector3d.from (SketchPlane3d.originPoint viewPlane)
     in
     SketchPlane3d.normalAxis viewPlane
         |> Axis3d.translateBy translationVector
-        |> Axis3d.intersectionWithPlane (SketchPlane3d.toPlane plane)
-        |> Maybe.map (Point3d.projectInto plane)
+        |> Axis3d.intersectionWithPlane (SketchPlane3d.toPlane sourcePlane)
+        |> Maybe.map (Point3d.projectInto sourcePlane)
 
 
-includeNewRect :
-    Maybe { a | originPoint : Point2d Length.Meters TopLeftCoordinates, length : Length.Length }
-    -> List (Rectangle2d Length.Meters TopLeftCoordinates)
-    -> List (Rectangle2d Length.Meters TopLeftCoordinates)
-includeNewRect newRect rects =
+includeDrawnRect : Maybe DrawnRect -> List Rect -> List Rect
+includeDrawnRect newRect rects =
     case newRect of
         Nothing ->
             rects
 
-        Just { originPoint, length } ->
-            toRectangle originPoint length :: rects
+        Just { rect } ->
+            rect :: rects
 
 
 type TopLeftCoordinates
@@ -278,6 +294,11 @@ turnAxis =
 
 wheelCoefficient =
     0.3
+
+
+pixelDensity =
+    Pixels.pixels 96
+        |> Quantity.per (Length.inches 1)
 
 
 wheelDecoder : (Float -> Float -> msg) -> Decoder msg
